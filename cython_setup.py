5c2e69c2f4249b2d440872d6429c839b033c96ea
tbloch1 <34276374+tbloch1@users.noreply.github.com>
2021-11-16 15:49:46 +0000

    Update cython_setup.py

--- cython_setup.py
+++ cython_setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize('vcurve_fns.pyx'))
+setup(ext_modules = cythonize('vcurve_fns.pyx'),
+      include_dirs = [numpy.get_include()])
